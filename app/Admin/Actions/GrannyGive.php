221bd434d2a3f0f351436743756ec4475b0abf9a
artyom kirnev <kirnevartyom30@gmail.com>
2022-03-14 01:19:16 +0200

    fixes

--- app/Admin/Actions/GrannyGive.php
+++ app/Admin/Actions/GrannyGive.php
@@ -1,17 +1,15 @@
 <?php
 
 namespace App\Admin\Actions;
+use App\Models\Granny;
 use Encore\Admin\Actions\RowAction;
-use Illuminate\Database\Eloquent\Model;
 
 class GrannyGive extends RowAction
 {
     public $name = 'Передать помощь бабушке';
-    public function handle(Model $model)
+    public function handle(Granny $model)
     {
-        $model->count +=1;
-        $model->save();
-
+        $model->helpGiven()->create();
         return $this->response()->success('Переданная помощь записана')->refresh();
     }
 }
--- app/Models/Granny.php
+++ app/Models/Granny.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Granny extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','address','phone',];
+    protected $fillable = ['granny_name', 'granny_phone', 'address', 'passport_id'];
+
+    public function helpGiven():HasMany
+    {
+        return $this->hasMany(HelpGiven::class);
+    }
 }
