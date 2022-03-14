a36e8c99ad9c4b5a77fd7f6bf437489c3965336f
arti <kirnevartem30@gmail.com>
2022-03-14 17:07:20 +0200

    db

--- app/Models/HelpGiven.php
+++ app/Models/HelpGiven.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class HelpGiven extends Model
 {
+    public $timestamps = false;
     use HasFactory;
-
+    protected $fillable = ['hg_timestamp', 'granny_id'];
     public function granny():BelongsTo
     {
         return $this->belongsTo(Granny::class);
