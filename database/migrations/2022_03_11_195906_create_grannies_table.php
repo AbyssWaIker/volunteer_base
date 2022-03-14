6e7690cc3cc2e425a88ce43a4e5b15dfe3c09eb9
arti <kirnevartem30@gmail.com>
2022-03-14 23:49:08 +0200

    db fixes

--- database/migrations/2022_03_11_195906_create_grannies_table.php
+++ database/migrations/2022_03_11_195906_create_grannies_table.php
@@ -14,6 +14,8 @@ class CreateGranniesTable extends Migration
     public function up()
     {
         Schema::create('grannies', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'UTF8_general_ci';
             $table->id();
             $table->string('granny_name');
             $table->string('address')->nullable();
