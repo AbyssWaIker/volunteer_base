cd87138a67910134d7b7e0d746fe78a8579895ca
arti <kirnevartem30@gmail.com>
2022-03-15 13:08:22 +0200

    база

--- app/Admin/Actions/GrannyGive.php
+++ app/Admin/Actions/GrannyGive.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Actions;
 use App\Models\Granny;
+use Carbon\Carbon;
 use Encore\Admin\Actions\RowAction;
 
 class GrannyGive extends RowAction
@@ -9,7 +10,7 @@ class GrannyGive extends RowAction
     public $name = 'Помочь бабушке';
     public function handle(Granny $model)
     {
-        $model->helpGiven()->create();
+        $model->helpGiven()->create(['hg_timestamp' => Carbon::now()]);
         return $this->response()->success('Переданная помощь записана')->refresh();
     }
 }
