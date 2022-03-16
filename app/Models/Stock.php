23fec0976d6f858c0ecbfdbe53373072ffe5d249
arti <kirnevartem30@gmail.com>
2022-03-16 16:16:25 +0200

    fix

---
+++ app/Models/Stock.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Stock extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name'];
+
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function quantityUnit():BelongsToMany
+    {
+        return $this->belongsToMany(QuantityUnit::class);
+    }
+}
