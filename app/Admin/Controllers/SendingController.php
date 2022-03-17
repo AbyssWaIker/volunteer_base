462343d5fd9b6c902313da54543299e5d68cdbbd
arti <kirnevartem30@gmail.com>
2022-03-17 11:24:26 +0200

    отправка готова

--- app/Admin/Controllers/SendingController.php
+++ app/Admin/Controllers/SendingController.php
@@ -5,13 +5,13 @@ namespace App\Admin\Controllers;
 use App\Models\QuantityUnit;
 use App\Models\ReceivingPoint;
 use App\Models\Stock;
+use Encore\Admin\Admin;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 use \App\Models\Sending;
-use Illuminate\Support\Facades\Request;
 
 class SendingController extends AdminController
 {
@@ -72,42 +72,39 @@ class SendingController extends AdminController
      */
     protected function form($id = 0)
     {
-        $model = $id ? Sending::query()->firstOrNew(['id'=>$id]): new Sending();
+        $model = Sending::query()->findOrNew($id);
         $form = new Form($model);
-        $receiving_point = ReceivingPoint::query()->pluck('name', 'id');
-        $stocks = Stock::query()->pluck('name', 'id');
-        $quantity_units = QuantityUnit::query()->pluck('name', 'id');
-
-        $current_receiving_point = @$receiving_point[$model->receiving_point_id];
-        $form->hidden('receiving_point_id')
-            ->value(@$model->receiving_point_id);
-        $form->text('receivingPointString', __('Receiving point'))
-            ->datalist($receiving_point)
-            ->default($current_receiving_point);
-        $form->hasMany('stocksSent', 'Отправленно', function (Form\NestedForm $form) use($stocks, $quantity_units) {
-            $form->text('stock_id');
-            $form->text('quantity_unit_id');
-            $form->text('stock')->datalist($stocks);
-            $form->text('quantity_unit')->datalist($quantity_units);
+        $receiving_point = ReceivingPoint::query()->pluck('name', 'id')->toArray();
+        $stocks = Stock::query()->pluck('name', 'id')->toArray();
+        $quantity_units = QuantityUnit::query()->pluck('name', 'id')->toArray();
+        $getValueSetter = function(array $options) {
+            return function(?string $value, Form\Field\Text $field) use ($options){
+                $true_value = @$options[$value];
+                $field->attribute('value', $true_value)->datalist($options)->value($true_value);
+                return $true_value;
+            };
+        };
+        $form->text('receiving_point_id')->with($getValueSetter($receiving_point));
+        $form->hasMany('stocksSent', 'Отправленно', function (Form\NestedForm $form) use($stocks, $quantity_units, $getValueSetter) {
+            $form->text('stock_id')->with($getValueSetter($stocks));
+            $form->text('quantity_unit_id')->with($getValueSetter($quantity_units));
             $form->number('quantity')->default(0);
 
         });
 
         $form->submitted(function (\Encore\Admin\Form $form) {
-
-            $rps = \request('receivingPointString');
-            $receiving_point = ReceivingPoint::query()->firstOrCreate(['name'=> $rps]);
+            $receiving_point = ReceivingPoint::query()->firstOrCreate(['name'=> \request('receiving_point_id')]);
             $stock_sent = \request('stocksSent');
             foreach ($stock_sent as &$item) {
-                $stock = Stock::query()->firstOrCreate(['name' => $item['stockString']]);
+                $stock = Stock::query()->firstOrCreate(['name' => $item['stock_id']]);
                 $item['stock_id'] = $stock->id;
 
-                $stock = QuantityUnit::query()->firstOrCreate(['name' => $item['quantityUnitString']]);
+                $stock = QuantityUnit::query()->firstOrCreate(['name' => $item['quantity_unit_id']]);
                 $item['quantity_unit_id'] = $stock->id;
             }
             $form->receiving_point_id = $receiving_point->id;
             $form->stocksSent = $stock_sent;
-//            \request()->merge(['stocksSent'=>$stock_sent,'receiving_point_id'=>$receiving_point->id]);
+            \request()->merge(['stocksSent'=>$stock_sent,'receiving_point_id'=>$receiving_point->id]);
 //            dd(\request());
         });
         return $form;
