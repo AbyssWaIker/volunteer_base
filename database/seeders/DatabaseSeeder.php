a9b99b988eae1846710de7db649c577f687a304b
arti <kirnevartem30@gmail.com>
2022-03-19 15:14:11 +0200

    посев категорий

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(DestituteCategoriesTableSeeder::class);
         $this->call(DestitutesTableSeeder::class);
+        $this->call(DestituteDestituteCategoryTableSeeder::class);
         $this->call(HelpGivensTableSeeder::class);
         $this->call(AdminUsersTableSeeder::class);
         $this->call(AdminRolesTableSeeder::class);
@@ -31,18 +33,16 @@ class DatabaseSeeder extends Seeder
         $this->call(PersonalAccessTokensTableSeeder::class);
         $this->call(QuantityUnitsTableSeeder::class);
         $this->call(ReceivingPointsTableSeeder::class);
-        $this->call(SendingsTableSeeder::class);
+        $this->call(ReceivingPointsTableSeeder::class);
         $this->call(StockCategoriesTableSeeder::class);
         $this->call(StocksTableSeeder::class);
+        $this->call(QuantityUnitsTableSeeder::class);
+        $this->call(SendingsTableSeeder::class);
         $this->call(StocksSentsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(DestituteCategoriesTableSeeder::class);
         $this->call(AdminOperationLogTableSeeder::class);
         $this->call(AdminRoleMenuTableSeeder::class);
-        $this->call(DestituteDestituteCategoryTableSeeder::class);
         $this->call(PersonalAccessTokensTableSeeder::class);
-        $this->call(QuantityUnitsTableSeeder::class);
-        $this->call(ReceivingPointsTableSeeder::class);
         $this->call(SendingsTableSeeder::class);
         $this->call(StockCategoriesTableSeeder::class);
         $this->call(StocksTableSeeder::class);
