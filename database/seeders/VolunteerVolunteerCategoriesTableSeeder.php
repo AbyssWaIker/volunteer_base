2c4166ef4783548688416c686dfe4f50e8df6409
arti <kirnevartem30@gmail.com>
2022-03-20 09:40:26 +0000

    db

---
+++ database/seeders/VolunteerVolunteerCategoriesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class VolunteerVolunteerCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Auto generated seeder file.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('volunteer_volunteer_category')->delete();
+        
+
+        
+    }
+}
