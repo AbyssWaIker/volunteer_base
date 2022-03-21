1cc26effe577a5c50368815eb56c3082a84219fa
arti <kirnevartem30@gmail.com>
2022-03-21 22:39:35 +0200

    get all ip addresses logs with geo

--- app/Console/Commands/GetAllIpAddressesList.php
+++ app/Console/Commands/GetAllIpAddressesList.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Support\Facades\DB;
+use Stevebauman\Location\Facades\Location;
+use Stevebauman\Location\Position;
 
 class GetAllIpAddressesList extends \Illuminate\Console\Command
 {
@@ -22,16 +24,32 @@ class GetAllIpAddressesList extends \Illuminate\Console\Command
      */
     public function handle()
     {
+        $addresses_cache = [];
+        $getIpPosition = function(string $ip) use (&$addresses_cache)/*:Position | string*/ {
+            if(key_exists($ip, $addresses_cache)) {
+                return $addresses_cache[$ip];
+            }
+            $addresses_cache[$ip] = Location::get($ip) ?: $ip;
+            return $addresses_cache[$ip] ?: $ip;
+        };
+        $positionToString = function (/*:Position | string*/ $p):string {
+            if(is_string($p)) {
+                return $p . ' - ?';
+            }
+            return "{$p->ip} - {$p->countryName}, {$p->regionName}, {$p->cityName}; lat: {$p->latitude}, long: {$p->longitude}";
+        };
         $logs = DB::table('admin_operation_log')->get([DB::raw('Date(created_at) as date'), 'ip']);
+        $unique_ips = $logs->unique('ip')->map->ip->map($getIpPosition)->map($positionToString)->implode(PHP_EOL);
+
         $new_log_for_unique_ips = fopen('unique_ips.txt', 'w');
-        fwrite($new_log_for_unique_ips, $logs->unique('ip')->implode('ip', PHP_EOL));
+        fwrite($new_log_for_unique_ips, $unique_ips);
         fclose($new_log_for_unique_ips);
 
-        $days = $logs->groupBy('date');
         $text = '';
+        $days = $logs->groupBy('date');
         foreach ($days as $day => $connections) {
             $text .= PHP_EOL . $day . PHP_EOL;
-            $text .= $connections->groupBy('ip')->keys()->implode(PHP_EOL) . PHP_EOL;
+            $text .= $connections->unique('ip')->map->ip->map($getIpPosition)->map($positionToString)->implode(PHP_EOL) . PHP_EOL;
         }
         $new_log_for_full_logs = fopen('full_logs.txt', 'w');
         fwrite($new_log_for_full_logs, $text);
