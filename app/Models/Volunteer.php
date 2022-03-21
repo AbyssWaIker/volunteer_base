dc19fbec2e4409f0c34fe3496c4155b0600ca6c1
arti <kirnevartem30@gmail.com>
2022-03-21 14:04:25 +0200

    Разделение по полу

--- app/Models/Volunteer.php
+++ app/Models/Volunteer.php
@@ -7,8 +7,16 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Volunteer extends Model
 {
-    protected $fillable = ['comment', 'name', 'phone'];
     use HasFactory;
+    protected $fillable = ['comment', 'name', 'phone', 'volunteer'];
+    public const SEX_MALE = 1;
+    public const SEX_FEMALE = 2;
+    public const SEX_MULTIPLE = 3;
+    public const SEX_OPTIONS = [
+        self::SEX_MALE => 'Мужчина',
+        self::SEX_FEMALE => 'Женщина',
+        self::SEX_MULTIPLE => 'Несколько человек',
+    ];
     public function categories():BelongsToMany
     {
         return $this->belongsToMany(VolunteerCategory::class);
