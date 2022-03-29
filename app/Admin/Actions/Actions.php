68cd80f8f0ff1f284593a6dee208ddf704d02f5e
arti <kirnevartem30@gmail.com>
2022-03-29 12:10:04 +0300

    действия

--- app/Admin/Actions/Actions.php
+++ app/Admin/Actions/Actions.php
@@ -5,66 +5,47 @@ namespace App\Admin\Actions;
 
 use App\Admin\Actions\DefaultActions\Delete;
 use App\Admin\Actions\DefaultActions\Edit;
-use App\Admin\Actions\DefaultActions\Show;
+use Encore\Admin\Admin;
 use Encore\Admin\Grid\Displayers\DropdownActions;
 
 class Actions extends DropdownActions
 {
-    protected $view = 'admin::grid.actions.dropdown';
+    protected $view = 'admin.grid.actions.actions';
 
-    protected $defaultClass = [Edit::class, Show::class, Delete::class];
+    protected $defaultClass = [Edit::class, Delete::class];
+    protected $custom_prepends = [];
 
-    /**
-     * Disable view action.
-     *
-     * @param bool $disable
-     *
-     * @return $this
-     */
-    public function disableView(bool $disable = true)
+    public function prepend($action):self
     {
-        if ($disable) {
-            array_delete($this->defaultClass, Show::class);
-        } elseif (!in_array(Show::class, $this->defaultClass)) {
-            array_push($this->defaultClass, Show::class);
-        }
+        $this->prepareAction($action);
 
+        array_push($this->custom_prepends, $action);
         return $this;
     }
 
     /**
-     * Disable delete.
+     * @param null|\Closure $callback
      *
-     * @param bool $disable
-     *
-     * @return $this.
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View|string
      */
-    public function disableDelete(bool $disable = true)
+    public function display($callback = null)
     {
-        if ($disable) {
-            array_delete($this->defaultClass, Delete::class);
-        } elseif (!in_array(Delete::class, $this->defaultClass)) {
-            array_push($this->defaultClass, Delete::class);
+        if ($callback instanceof \Closure) {
+            $callback->call($this, $this);
         }
-
-        return $this;
+        if ($this->disableAll) {
+            return '';
+        }
+        $this->prependDefaultActions();
+        $variables = [
+            'custom_prepends' => $this->custom_prepends,
+            'default' => $this->default,
+            'custom'  => $this->custom,
+        ];
+        if (empty($variables['default']) && empty($variables['custom'])) {
+            return '';
+        }
+        return Admin::component($this->view, $variables);
     }
 
-    /**
-     * Disable edit.
-     *
-     * @param bool $disable
-     *
-     * @return $this
-     */
-    public function disableEdit(bool $disable = true)
-    {
-        if ($disable) {
-            array_delete($this->defaultClass, Edit::class);
-        } elseif (!in_array(Edit::class, $this->defaultClass)) {
-            array_push($this->defaultClass, Edit::class);
-        }
-
-        return $this;
-    }
 }
