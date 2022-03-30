cc431f71ccc5cbdf6e198592a99880ee966affbe
arti <kirnevartem30@gmail.com>
2022-03-30 15:02:09 +0300

    статистика по отправкам

--- app/Admin/Controllers/StockByCategoryController.php
+++ app/Admin/Controllers/StockByCategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Models\Stock;
+use App\Models\StocksSent;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
@@ -25,10 +26,22 @@ abstract class StockByCategoryController extends AdminController
     protected function grid()
     {
         $grid = parent::grid();
-        $grid->model()->where('category_id', $this->category_id)->orderByDesc('deficit');
+        $grid->model()/*->where('category_id', $this->category_id)*/->orderByDesc('deficit_status');
         $grid->column('id', __('Id'));
-        $grid->column('name', __('Name'));
-        $grid->column('deficit', __('Deficit'));
+        $grid->column('name', __('Name'))->expand(function (Stock $stock) {
+            $days = $stock->sentStocks->groupBy(function (StocksSent $sentStock){ return $sentStock->created_at->toDateString();});
+            $resulting_html = '';
+            foreach ($days as $day=>$stocks) {
+                $resulting_html .= <<<HTML
+<h3>$day</h3>
+<ul>
+{$stocks->map(function (StocksSent $sent): string {return "<li>{$sent->getLineForStats()}</li>";})->implode('<br/>')}
+</ul>
+HTML;
+            }
+            return $resulting_html;
+        });
+        $grid->column('deficit_status', __('Deficit'))->using(Stock::DEFICIT_STATUS_OPTIONS);
         $grid->column('category.name', __('Category'));
         $grid->column('created_at', __('Created at'));
         $grid->column('updated_at', __('Updated at'));
@@ -48,7 +61,7 @@ abstract class StockByCategoryController extends AdminController
 
         $show->field('id', __('Id'));
         $show->field('name', __('Name'));
-        $show->field('deficit', __('Deficit'));
+        $show->field('deficit_status', __('Deficit'));
         $show->field('category_id', __('Category id'));
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
@@ -66,7 +79,7 @@ abstract class StockByCategoryController extends AdminController
         $form = parent::form($id);
 
         $form->text('name', __('Name'));
-        $form->number('deficit', __('Deficit'));
+        $form->number('deficit_status', __('Deficit'));
         $form->hidden('category_id')->default($this->category_id);
 
         return $form;
