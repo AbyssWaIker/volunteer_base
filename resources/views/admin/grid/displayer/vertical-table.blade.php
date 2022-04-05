49bc5ec37e70df1503638de723be8c461b072fb2
arti <kirnevartem30@gmail.com>
2022-04-05 14:40:55 +0300

    vertical table

---
+++ resources/views/admin/grid/displayer/vertical-table.blade.php
@@ -0,0 +1,13 @@
+<table class="table table-hover" style="margin-bottom: 0;">
+    <tbody>
+    @foreach($data as $column => $datum)
+    <tr>
+
+        <td><b>{{ @$titles[$column] }}</b></td>
+        @foreach($datum as $key => $value)
+            <td>{{ $value }}</td>
+        @endforeach
+    </tr>
+    @endforeach
+    </tbody>
+</table>
