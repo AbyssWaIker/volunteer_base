5803ee989671dbeb6d4374a584b34a6ad7a9daa6
arti <kirnevartem30@gmail.com>
2022-04-05 15:43:17 +0300

    person controller abstraction part 1

--- app/Models/Volunteer.php
+++ app/Models/Volunteer.php
@@ -5,24 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Volunteer extends Model
+class Volunteer extends Person
 {
-    use HasFactory;
-    protected $fillable = ['comment', 'name', 'phone', 'volunteer'];
+    protected $categoryClass = VolunteerCategory::class;
+    protected $fillable = ['comment', 'name', 'phone', 'sex'];
     public const SEX_MALE = 0;
     public const SEX_FEMALE = 1;
 //    public const SEX_MULTIPLE = 2;
     public const SEX_SWITCH_STATES = [
-        'on' => ['value' => self::SEX_MALE, 'text' => 'Мужчина', 'color' => 'primary'],
-        'off' => ['value' => self::SEX_FEMALE, 'text' => 'Женщина', 'color' => 'success'],
+        'on' => ['value' => self::SEX_MALE, 'text' => self::SEX_OPTIONS[self::SEX_MALE], 'color' => 'primary'],
+        'off' => ['value' => self::SEX_FEMALE, 'text' => self::SEX_OPTIONS[self::SEX_FEMALE], 'color' => 'success'],
     ];
     public const SEX_OPTIONS = [
         self::SEX_MALE => 'Мужчина',
         self::SEX_FEMALE => 'Женщина',
 //        self::SEX_MULTIPLE => 'Несколько человек',
     ];
-    public function categories():BelongsToMany
+
+    public static function getTableTitles(): array
     {
-        return $this->belongsToMany(VolunteerCategory::class);
+        return array_merge(parent::getTableTitles(), ['sex' => __('Sex')]);
+    }
+
+    public function getTableInfoAttribute(): array
+    {
+        return array_merge(
+            parent::getTableInfoAttribute(),
+            ['sex' => @self::SEX_OPTIONS[$this->sex]?: ?: self::SEX_OPTIONS[self::SEX_MALE]]
+        );
     }
 }
