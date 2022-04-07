39ede8226f45e6379dfcbf0b9791aef4148332f5
arti <kirnevartem30@gmail.com>
2022-04-07 12:45:11 +0300

    fix

---
+++ database/seeders/AdminConfigTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AdminConfigTableSeeder extends Seeder
+{
+    /**
+     * Auto generated seeder file.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('admin_config')->delete();
+        
+        \DB::table('admin_config')->insert(array (
+            0 => 
+            array (
+                'id' => 2,
+                'name' => 'RECEIVER_TELEGRAM_ID',
+                'value' => '385305393',
+                'description' => 'id получателя отчетов',
+                'created_at' => '2022-03-29 11:43:03',
+                'updated_at' => '2022-03-29 12:32:01',
+            ),
+        ));
+
+        
+    }
+}
