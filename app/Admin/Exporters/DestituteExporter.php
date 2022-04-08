52e017db5939e9eb083e23e1eab19cf251c140d9
artyom kirnev <kirnevartyom30@gmail.com>
2022-04-09 00:01:13 +0300

    fix

--- app/Admin/Exporters/DestituteExporter.php
+++ app/Admin/Exporters/DestituteExporter.php
@@ -18,15 +18,15 @@ class DestituteExporter extends PeopleWithCategoriesExporter
     public function map($row): array
     {
         return [
-            'id' => '',
+            'id' => $row->id,
             'name' => $row->name,
             'phone' => ' '.$row->phone . ' ',
             'address' => $row->address,
             'passport_id' => ' '.$row->passport_id . ' ',
             'id_code' => ' '.$row->id_code . ' ',
             'comment' => $row->comment,
-            'helpGiven'=>Destitute::getHelpHistory($row->helpGiven->toArray(), false),
             'categories' => $row->categories->map(function ($category){return $category->name;})->implode(', '),
+            'helpGiven'=>Destitute::getHelpHistory($row->helpGiven->toArray(), false),
         ];
     }
 }
