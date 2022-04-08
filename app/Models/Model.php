0d5840bef2c9ada39248540a833077ec139e9082
artyom kirnev <kirnevartyom30@gmail.com>
2022-04-08 22:55:30 +0300

    streamlined translation

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -7,8 +7,24 @@ use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 class Model extends \Illuminate\Database\Eloquent\Model
 {
     use Cachable;
+    protected $appends = ['table_info'];
     static public function pluckNameAndID():array
     {
         return self::query()->pluck('name', 'id')->toArray();
+    }
+
+    public static function getTableTitles():array
+    {
+        $attributes = self::getFillable();
+        $result = [];
+        foreach ($attributes as $attribute) {
+            $result[$attribute] = __($attribute);
+        }
+        return $result;
+    }
+
+    public function getTableInfoAttribute():array
+    {
+        return $this->getAttributes();
     }
 }
