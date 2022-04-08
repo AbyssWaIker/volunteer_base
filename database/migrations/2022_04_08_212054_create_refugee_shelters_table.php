4e5a34254cef67d296bd6db46405ff3aa10fe91b
artyom kirnev <kirnevartyom30@gmail.com>
2022-04-09 00:31:46 +0300

    start for Refugee info gathering

---
+++ database/migrations/2022_04_08_212054_create_refugee_shelters_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRefugeeSheltersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('refugee_shelters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('terms')->nullable();
+            $table->string('vacant_places')->nullable();
+            $table->string('conditions')->nullable();
+            $table->string('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('refugee_shelters');
+    }
+}
