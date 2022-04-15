f0d091e2a0fb72354f9b29e70703aff5e9ebd485
arti <kirnevartem30@gmail.com>
2022-04-15 12:49:22 +0300

    ;

--- app/Admin/Controllers/DestitutesController.php
+++ app/Admin/Controllers/DestitutesController.php
@@ -130,7 +130,7 @@ class DestitutesController extends PersonController
         $form->text('comment', __('comment'));
         $form->table('family_members', __('family_members'), function(NestedForm $form) {
             $form->text('name',__('name'))->required();
-            $form->text('passport_id', __('passport_id'))
+            $form->text('passport_id', __('passport_id'));
         });
         $form->hasMany('helpGiven', 'Получила гуманитарную помощь', function(\Encore\Admin\Form\NestedForm $form){
             $form->hidden('id');
