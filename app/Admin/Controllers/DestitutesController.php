3af5f323421e1f4d28b3a880df015807f7bcb89c
arti <kirnevartem30@gmail.com>
2022-04-19 16:53:12 +0300

    pdf

--- app/Admin/Controllers/DestitutesController.php
+++ app/Admin/Controllers/DestitutesController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Admin\Actions\Destitute\DestituteHelper;
+use App\Admin\Actions\Destitute\DestitutePDF;
 use App\Admin\Exporters\DestituteExporter;
 use App\Models\Destitute;
 use Carbon\Carbon;
@@ -115,6 +116,7 @@ class DestitutesController extends PersonController
 
         $grid->actions(function(Grid\Displayers\Actions $actions) {
             $actions->disableView();
+            $actions->prepend(new DestitutePDF);
             $actions->prepend(new DestituteHelper);
         });
 
