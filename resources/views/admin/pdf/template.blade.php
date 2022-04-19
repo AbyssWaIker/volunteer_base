87545bbc9d5987eb413567901ec8d177b42d7c3c
arti <kirnevartem30@gmail.com>
2022-04-19 16:21:41 +0300

    fix

--- resources/views/admin/pdf/template.blade.php
+++ resources/views/admin/pdf/template.blade.php
@@ -4,6 +4,9 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	<style>
+		body { font-family: DejaVu Sans }
+	</style>
 </head>
 <body>
 	<div>
