0c46e87e77aa09348b006dd13db64bd1e7210364
arti <kirnevartem30@gmail.com>
2022-04-19 16:53:12 +0300

    pdf

---
+++ app/Admin/Actions/Destitute/DestitutePDF.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Admin\Actions\Destitute;
+use App\Admin\Actions\DefaultActions\RowAction;
+
+class DestitutePDF extends RowAction
+{
+    public $name = 'В PDF';
+
+    public function html()
+    {
+        return '<i class="fa fa-print"></i>';
+    }
+
+    /**
+     * @return string
+     */
+    public function href()
+    {
+        return "{$this->getResource()}/{$this->getKey()}/print-pdf";
+    }
+}
