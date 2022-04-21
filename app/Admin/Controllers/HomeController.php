6fdf68e4d8e07e54dbafa974cb33e99236b4e4a5
artyom kirnev <kirnevartyom30@gmail.com>
2022-04-21 14:48:21 +0300

    главная страница

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -3,32 +3,31 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use Encore\Admin\Auth\Database\Menu;
 use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
+use Encore\Admin\Widgets\InfoBox;
 
 class HomeController extends Controller
 {
     public function index(Content $content)
     {
         return $content
-            ->title('Dashboard')
-            ->description('Description...')
-            ->row(Dashboard::title())
+            ->title(__('Main Page'))
+//            ->description('Description...')
+//            ->row(Dashboard::title())
             ->row(function (Row $row) {
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
+                $menus = Menu::all()->filter(function (Menu $menu) {
+                    return $menu->uri && $menu->uri !== '/' && $menu->parent_id != 2;
                 });
+                foreach ($menus as $menu) {
+                    $row->column(4, function (Column $column) use ($menu) {
+                        $column->append(new InfoBox($menu->title, substr($menu->icon, 3), 'aqua', config('admin.route.prefix').'/'.$menu->uri, ''));
+                    });
+                }
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
-                });
             });
     }
 }
