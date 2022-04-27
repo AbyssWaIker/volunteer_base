5d4d107b720e9b1f0795d9aa62e9fafffed3ae50
arti <kirnevartem30@gmail.com>
2022-04-27 12:55:19 +0300

    fixes

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
                     }
                     $menu = Menu::find($parent_id);
                     $links = $group->map(function(Menu $menu) {
-                        return '<a href="'.config('admin.route.prefix').'/'.$menu->uri.'">'.$menu->title.'</a>'
+                        return '<a href="'.config('admin.route.prefix').'/'.$menu->uri.'">'.$menu->title.'</a>';
                     })->implode('<br/>');
                     $row->column(4, function (Column $column) use ($menu) {
 
