67938030d04d9163155ce2f23e3a857dcfed6033
arti <kirnevartem30@gmail.com>
2022-05-05 11:11:01 +0300

    fix

--- database/migrations/2022_05_03_115244_add_comment copy.php
+++ database/migrations/2022_05_03_115244_add_comment copy.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddComment extends Migration
+class MakeCommentNullable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddComment extends Migration
     public function up()
     {
         Schema::table('destitutes', function (Blueprint $table) {
-            $table->string('comment');
+            $table->string('comment')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddComment extends Migration
     public function down()
     {
         Schema::table('destitutes', function (Blueprint $table) {
-            $table->dropColumn('comment');
+            $table->string('comment')->nullable(false);
         });
     }
 }
