2393a83718d5d0d36b7e228fb1ae6f5561fad868
arti <kirnevartem30@gmail.com>
2022-05-12 11:21:38 +0300

    test

--- app/Models/Destitute.php
+++ app/Models/Destitute.php
@@ -33,7 +33,7 @@ class Destitute extends Person
             }
             $value['name'] = mb_convert_case($value['name'], MB_CASE_TITLE);
             $value['passport_id'] = mb_convert_case($value['passport_id'], MB_CASE_UPPER);
-            $value['reference_id'] = $value['reference_id'] ?: __('reference is being processed');
+            $value['reference_id'] = @$value['reference_id'] ?: __('reference is being processed');
             if(str_starts_with('І-',$value['id_code'])) {
                 $value['is_child'] = true;
             }
