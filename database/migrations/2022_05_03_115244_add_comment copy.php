51cd393eea02c89ae2e897fb0fca64ce930371c7
artyom kirnev <kirnevartyom30@gmail.com>
2022-05-16 21:27:21 +0300

    fix

--- database/migrations/2022_05_03_115244_add_comment copy.php
+++ database/migrations/2022_05_03_115244_add_comment copy.php
@@ -4,11 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-<<<<<<< HEAD:database/migrations/2022_05_03_115244_add_comment copy.php
-class MakeComment extends Migration
-=======
 class AddCommentCopy extends Migration
->>>>>>> 2b00b683ad037aa0551780383d23816dfc301cf8:database/migrations/2022_03_12_173722_create_sendings_table.php
 {
     /**
      * Run the migrations.
