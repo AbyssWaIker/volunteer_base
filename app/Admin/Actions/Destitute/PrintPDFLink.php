e315a1c17c090f68e7f4e0505ef2deddec170184
artyom kirnev <kirnevartyom30@gmail.com>
2022-05-25 00:35:54 +0300

    pdf

--- app/Admin/Actions/Destitute/PrintPDFLink.php
+++ app/Admin/Actions/Destitute/PrintPDFLink.php
@@ -2,45 +2,63 @@
 
 namespace App\Admin\Actions\Destitute;
 use App\Admin\Actions\DefaultActions\RowAction;
+use App\Models\Destitute;
+use Encore\Admin\Admin;
+use Jenssegers\Date\Date;
 
 class PrintPDFLink extends RowAction
 {
     public $name = 'В PDF';
     protected $href_target = '_blank';
+    protected $wrapper_class = 'print';
 
     public function html()
     {
-
-        $destitute = \App\Models\Destitute::findOrFail($this->row);
+        $pdf = view('admin.pdf.template', $this->getData())->render();
+        Admin::js(asset('js/print.js'));
+        Admin::script($this->script());
+        $route = route(admin_get_route('print-pdf'), ['id'=>$this->getKey()]);
+        return <<<HTML
+        <i class="fa fa-print"></i>
+        <div class='hidden'>
+            <iframe src="$route" title="{$this->row->name}" id="{$this->wrapper_class}-{$this->getKey()}" name="{$this->wrapper_class}-{$this->getKey()}">
+                {$pdf}
+            </iframe>
+        </div>
+        HTML;
+    }
+    protected function getData():array
+    {
+        $destitute = $this->row;
+        if(!$destitute instanceof Destitute){
+            throw new \Error(__("Could not find data about destitute {$this->getKey()}"));
+        }
         $helpGiven = $destitute->helpGiven->last();
 
         $list = \App\Models\Stock::query()->where('enabled', true)->pluck('name');
-        $data = [
+        return [
             'dest' => $destitute,
-            'date' => Jenssegers\Date\Date::parse($helpGiven->hg_timestamp)->locale('uk','ru'),
+            'date' => Date::parse($helpGiven->hg_timestamp)->locale('uk','ru'),
             'min_number_of_rows' => 20,
             'list' => $list,
             'list_count' => count($list),
         ];
-        // return view('admin.pdf.template', $data)->render();
-        $pdf = view('admin.pdf.template', $data)->render();
-
-        return <<<HTML 
-        <i class="fa fa-print"></i>
-        <div class='hidden'>
-            <div id="print-{$this->row}">
-                {$pdf}
-            </div>
-
-        </div>
-        HTML;
     }
     public function script():string
     {
         return <<<JS
-        $('#print-{$this->row}').click((ev)=>{
-            console.log('presss');    
-        })
+        $('.{$this->getElementClass()}').off('click').on('click', (ev)=>{
+            const iframe = window.frames['{$this->wrapper_class}-{$this->getKey()}'];
+            iframe.focus();
+            iframe.print();
+            // window.frames['{$this->wrapper_class}-{$this->getKey()}'].focus();
+            // setTimeout(()=>window.frames['{$this->wrapper_class}-{$this->getKey()}'].print(), 1000);
+            // window.frames['{$this->wrapper_class}-{$this->getKey()}'].print();
+        });
         JS;
+    }
+    public function handle()
+    {
+        return true;
     }
 }
