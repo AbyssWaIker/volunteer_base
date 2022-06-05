6cbba5b001dfafde99ad1829fa51211e1f48e4b8
artyom kirnev <kirnevartyom30@gmail.com>
2022-06-05 22:09:54 +0300

    fix

--- remove_file_from_git_history.sh
+++ remove_file_from_git_history.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch $1' \--prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter \
+"git rm --cached --ignore-unmatch $1" \
+--prune-empty --tag-name-filter cat -- --all
