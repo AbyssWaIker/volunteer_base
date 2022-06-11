604c4fbba69fbc1aa032a3f1092f76916166a188
artyom kirnev <kirnevartyom30@gmail.com>
2022-06-11 22:47:49 +0300

    print code clean up

--- app/Admin/Actions/Destitute/PrintPDFForMedicalLink.php
+++ app/Admin/Actions/Destitute/PrintPDFForMedicalLink.php
@@ -7,7 +7,6 @@ use Encore\Admin\Admin;
 class PrintPDFForMedicalLink extends PrintPDFLink
 {
     public $name = 'Распечатать медицинский акт';
-    protected $href_target = '_blank';
     protected $wrapper_class = 'print-for-med';
     protected $icon = 'fa-plus-square';
     protected const DEFAULT_LIST_TYPE = self::LIST_SKIP;
