9d5c7b74ee58053a1525900983efaad989ca2745
arti <kirnevartem30@gmail.com>
2022-06-23 23:08:35 +0000

    fixes

--- app/Models/Volunteer.php
+++ app/Models/Volunteer.php
@@ -13,25 +13,15 @@ class Volunteer extends Person
     public const SEX_MALE = 0;
     public const SEX_FEMALE = 1;
 //    public const SEX_MULTIPLE = 2;
-<<<<<<< HEAD
-    public const SEX_SWITCH_STATES = [
-        'off' => ['value' => self::SEX_MALE, 'text' => self::SEX_OPTIONS[self::SEX_MALE], 'color' => 'primary'],
-        'on' => ['value' => self::SEX_FEMALE, 'text' => self::SEX_OPTIONS[self::SEX_FEMALE], 'color' => 'success'],
-    ];
-=======
->>>>>>> 0e56d897e4a333d4f5c4540d48a1196b4c3dd221
     public const SEX_OPTIONS = [
         self::SEX_MALE => 'Мужчина',
         self::SEX_FEMALE => 'Женщина',
 //        self::SEX_MULTIPLE => 'Несколько человек',
     ];
-<<<<<<< HEAD
-=======
     public const SEX_SWITCH_STATES = [
         'off' => ['value' => self::SEX_MALE, 'text' => self::SEX_OPTIONS[self::SEX_MALE], 'color' => 'primary'],
         'on' => ['value' => self::SEX_FEMALE, 'text' => self::SEX_OPTIONS[self::SEX_FEMALE], 'color' => 'success'],
     ];
->>>>>>> 0e56d897e4a333d4f5c4540d48a1196b4c3dd221
     public function getTableInfoAttribute(): array
     {
         return array_merge(
