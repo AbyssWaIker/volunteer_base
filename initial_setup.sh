5789024b4f46b6c659f25ba4e8dca9a4300047c0
arti <kirnevartem30@gmail.com>
2022-07-01 22:30:57 +0300

    initial setup added cron + optimisations

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 
-composer install
+docker-compose exec app composer install
 
-php artisan vendor:publish --all
-php artisan migrate:fresh --seed
+docker-compose exec app php artisan vendor:publish --all
+docker-compose exec app php artisan migrate:fresh --seed
 
-php artisan key:generate
+docker-compose exec app php artisan key:generate
+
+echo "0 16 * * * docker-compose exec app php artisan db:backup" > /tmp/crontab
+crontab /tmp/crontab
+rm /tmp/crontab
