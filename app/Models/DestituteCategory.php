f27d49fcdca362518a28236d84c4920d8b7f92ab
arti <kirnevartem30@gmail.com>
2022-07-02 21:13:57 +0300

    fix

--- app/Models/DestituteCategory.php
+++ app/Models/DestituteCategory.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class DestituteCategory extends Model
+class DestituteCategory extends Category
 {
     public $timestamps = false;
 
