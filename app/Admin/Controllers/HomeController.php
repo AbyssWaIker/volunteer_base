df5a5baccdb49fcc11051973ebdba96162e89ce8
artyom kirnev <kirnevartyom30@gmail.com>
2022-07-04 01:45:59 +0300

    permissions

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
 use Encore\Admin\Auth\Database\Menu;
+use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
@@ -16,12 +17,16 @@ class HomeController extends Controller
         return $content
             ->title(__('Main Page'))
             ->row(function (Row $row) {
-                $menus = Menu::all()->filter(function (Menu $menu) {
-                    return $menu->uri && $menu->uri !== '/' && $menu->parent_id != 2;
+                $menus = array_filter(Admin::menu(), function (array $menu) {
+                    return $menu['uri'] 
+                        && $menu['uri'] !== '/' 
+                        && $menu['parent_id'] == null 
+                        && Admin::user()->visible(\Illuminate\Support\Arr::get($menu, 'roles', [])) 
+                        && Admin::user()->can(\Illuminate\Support\Arr::get($menu, 'permission'));
                 });
                 foreach ($menus as $menu) {
                     $row->column(4, function (Column $column) use ($menu) {
-                        $column->append(new InfoBox('', substr($menu->icon, 3), 'aqua', config('admin.route.prefix').'/'.$menu->uri, $menu->title));
+                        $column->append(new InfoBox('', substr($menu['icon'], 3), 'aqua', config('admin.route.prefix').'/'.$menu['uri'], $menu['title']));
                     });
                 }
 
