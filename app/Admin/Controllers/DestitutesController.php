e56dee9de431f1ef4c83fdd6bac5c732d0a0a6e5
artyom kirnev <kirnevartyom30@gmail.com>
2022-07-19 15:30:44 +0300

    фильтры

--- app/Admin/Controllers/DestitutesController.php
+++ app/Admin/Controllers/DestitutesController.php
@@ -89,9 +89,14 @@ class DestitutesController extends PersonController
 
                 $filter->where(function(Builder $query) {
                     $query->whereHas('helpGiven', function (Builder $query) {
-                        $query->whereDate('hg_timestamp', $this->input);
+                        $query->whereDate('hg_timestamp', '>=',$this->input);
                     });
-                }, 'Дата', 'date')->date();
+                }, 'Дата с', 'date_start')->date();
+                $filter->where(function(Builder $query) {
+                    $query->whereHas('helpGiven', function (Builder $query) {
+                        $query->whereDate('hg_timestamp', '=<',$this->input);
+                    });
+                }, 'Дата по', 'date_end')->date();
 
                 // $filter->where(function($query) {
                 //     switch($this->input) {
